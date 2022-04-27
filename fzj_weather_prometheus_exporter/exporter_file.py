886dda8ceb4dcec6211f4f9becf46075624395b3
odruska <oskar.druska@alumni.fh-aachen.de>
2022-04-27 11:33:02 +0200

    adds positional parameters to exporter gauges specifies time summary

--- fzj_weather_prometheus_exporter/exporter_file.py
+++ fzj_weather_prometheus_exporter/exporter_file.py
@@ -3,51 +3,56 @@
 from prometheus_client import Summary
 from prometheus_client.core import GaugeMetricFamily
 
-from fzj_weather_crawler import fzj_weather_crawler
+from fzj_weather_prometheus_exporter import fzj_weather_crawler
 
-REQUEST_TIME = Summary()
+REQUEST_TIME = Summary("weather_exporter_collect_seconds",
+                       "Time spent to collect metrics from fzj_weather.py")
 
 
 class FZJWeatherExporter:
 
     @REQUEST_TIME.time()
     def collect(self):
-        weather = fzj_weather_crawler()
+        weather = fzj_weather_crawler.fzj_weather_crawler()
 
         g = GaugeMetricFamily(
-            'fzjweather_temperature_celcius', 'temperature in celcius')
+            name='fzj_weather_air_temperature_celsius',
+            documentation='temperature in celcius')
         g.add_metric([], weather.temperature)
         yield g
 
         g = GaugeMetricFamily(
-            'fzjweather_air_pressure_hectopascal',
-            'air pressure in hectopascal')
+            name='fzj_weather_air_pressure_hectopascal',
+            documentation='air pressure in hectopascal')
         g.add_metric([], weather.air_pressure)
         yield g
 
         g = GaugeMetricFamily(
-            'fzjweather_humidity_percent', 'humidity in percent')
+            name='fzj_weather_humidity_percent',
+            documentation='humidity in percent')
         g.add_metric([], weather.humidity)
         yield g
 
         g = GaugeMetricFamily(
-            'fzjweather_wind_power_beaufort', 'wind power in beaufort')
+            name='fzj_weather_wind_power_beaufort',
+            documentation='wind power in beaufort')
         g.add_metric([], weather.wind_power)
         yield g
 
         g = GaugeMetricFamily(
-            'fzjweather_wind_direction_degree', 'wind direction in degree')
+            name='fzj_weather_wind_direction_degree',
+            documentation='wind direction in degree')
         g.add_metric([], weather.wind_direction)
         yield g
 
         g = GaugeMetricFamily(
-            'fzjweather_velocity_meter_per_second',
-            'wind velocity in metres per second')
+            name='fzj_weather_velocity_meters_per_second',
+            documentation='wind velocity in meters per second')
         g.add_metric([], weather.velocity_ms)
         yield g
 
         g = GaugeMetricFamily(
-            'fzjweather_velocity_kilometer_per_hour',
-            'wind velocity in kilometres per hour')
+            name='fzj_weather_velocity_kilometers_per_hour',
+            documentation='wind velocity in kilometers per hour')
         g.add_metric([], weather.velocity_kmh)
         yield g
